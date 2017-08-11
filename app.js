0bf7f46edc5c14c39d13aeabd68e3c1c33b2411a
Vinod9600 <vinoddurge9600@gmail.com>
2017-08-11 09:11:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var bodyparser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://vinod9600:yJn7b7BN3RiSv5SE@contactlist-shard-00-00-26xeg.mongodb.net:27017,contactlist-shard-00-01-26xeg.mongodb.net:27017,contactlist-shard-00-02-26xeg.mongodb.net:27017/contactlist?ssl=true&replicaSet=ContactList-shard-0&authSource=admin";
 app.use(morgan('dev'));
 //adding middleware - cors
 app.use(cors());
@@ -15,17 +18,15 @@ app.use(bodyparser.urlencoded({ extended: true })); // for parsing application/x
 const route = require('./routes/route');
 
 //connect to mongodb
-mongoose.connect('mongodb://vinod9600:Abcd@9762@contactlist-shard-00-00-26xeg.mongodb.net:27017,contactlist-shard-00-01-26xeg.mongodb.net:27017,contactlist-shard-00-02-26xeg.mongodb.net:27017/contactlist?ssl=true&replicaSet=ContactList-shard-0&authSource=admin');
-
-mongoose.connection.on('connected',()=>{
-    console.log('connected to database mongodb @ 27017'); 
+MongoClient.connect(uri, function(err, db) {
+  db.close();
 });
-
+/*
 mongoose.connection.on('error',(err)=>{
     if(err){
         console.log('Error to connect to database:' + err);
     }
-});
+});*/
 app.use('/api', route);
 
 
